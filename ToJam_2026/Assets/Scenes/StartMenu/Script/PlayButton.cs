93d39e889923e33238b4cb4444bfc92064617a67
sin-sord <kevkinzz@gmail.com>
2026-05-10 15:43:09 -0400

    StartMenu Transitions to Game
    Updated:
    - script that takes player to scene: Decision 1

---
+++ ToJam_2026/Assets/Scenes/StartMenu/Script/PlayButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayBUtton : MonoBehaviour
+{
+   
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Decision 1");
+    }
+
+}
