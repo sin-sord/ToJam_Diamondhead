1fbc0fc4ec73528669e7c384b9a5e0216c923931
T4lynn <148375548+T4lynn@users.noreply.github.com>
2026-05-10 17:20:43 -0400

    UpdatedTitleScreen
    - added credits menu

--- ToJam_2026/Assets/Scenes/StartMenu/Script/PlayButton.cs
+++ ToJam_2026/Assets/Scenes/StartMenu/Script/PlayButton.cs
@@ -3,10 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class PlayBUtton : MonoBehaviour
 {
+    public GameObject creditsButton;
    
     public void PlayGame()
     {
         SceneManager.LoadScene("Decision 1");
     }
 
+    public void Credits()
+    {
+        creditsButton.SetActive(true); 
+    }
+
+    public void DisableCredits()
+        { creditsButton.SetActive(false); }
 }
