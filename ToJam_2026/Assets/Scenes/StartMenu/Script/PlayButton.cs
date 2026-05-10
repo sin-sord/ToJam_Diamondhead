139cd4b2f02d3df146ae9ce4afabd445bb27f6a5
T4lynn <148375548+T4lynn@users.noreply.github.com>
2026-05-10 17:51:37 -0400

    UpdatedTitlescreen
    - added title text
    - added controls button

--- ToJam_2026/Assets/Scenes/StartMenu/Script/PlayButton.cs
+++ ToJam_2026/Assets/Scenes/StartMenu/Script/PlayButton.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class PlayBUtton : MonoBehaviour
 {
     public GameObject creditsButton;
+    public GameObject controlsButton;
    
     public void PlayGame()
     {
@@ -17,4 +18,14 @@ public class PlayBUtton : MonoBehaviour
 
     public void DisableCredits()
         { creditsButton.SetActive(false); }
+
+    public void Controls()
+        { controlsButton.SetActive(true); }
+
+    public void DisableControls()
+    {
+        controlsButton.SetActive(false);
+    }
+
+
 }
